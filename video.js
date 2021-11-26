fc6ae5182cbd44c34748d9ad1ddac8fc272a2bfc
santiherrera99@icloud.com <santiherrera99@icloud.com>
2021-11-26 17:50:05 -0300

    videos

---
+++ video.js
@@ -0,0 +1,55 @@
+const btn = document.querySelector('.btn');
+
+const videoContainer = document.querySelector('.video-container');
+
+const close = document.querySelector('.close');
+
+btn.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    videoContainer.classList.remove('show');
+})
+
+const btn2 = document.querySelector('.btn2');
+
+const videoContainer2 = document.querySelector('.video-container2');
+
+const close2 = document.querySelector('.close2');
+
+btn2.addEventListener('click', () => {
+    videoContainer2.classList.add('show');
+})
+
+close2.addEventListener('click', () => {
+    videoContainer2.classList.remove('show');
+})
+
+const btn3 = document.querySelector('.btn3');
+
+const videoContainer3 = document.querySelector('.video-container3');
+
+const close3 = document.querySelector('.close3');
+
+btn3.addEventListener('click', () => {
+    videoContainer3.classList.add('show');
+})
+
+close3.addEventListener('click', () => {
+    videoContainer3.classList.remove('show');
+})
+
+const btn4 = document.querySelector('.btn4');
+
+const videoContainer4 = document.querySelector('.video-container4');
+
+const close4 = document.querySelector('.close4');
+
+btn4.addEventListener('click', () => {
+    videoContainer4.classList.add('show');
+})
+
+close4.addEventListener('click', () => {
+    videoContainer4.classList.remove('show');
+})
