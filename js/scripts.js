d6fd28b75762efad94e9b279de968bd604fa2c95
santiherrera99@icloud.com <santiherrera99@icloud.com>
2021-12-28 10:38:26 -0300

    menu

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,12 @@ jQuery('document').ready(function ($) {
         }
     })
 });
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('ul li a');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = "active"
+    }
+}
